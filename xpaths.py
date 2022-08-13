0d28e45011f50702e6c655dada243af7a58df90b
Wai Han <wh.kankan13@gmail.com>
2022-08-13 15:37:42 -0700

    fixed gitignore

--- xpaths.py
+++
@@ -1,9 +0,0 @@
-user_xpath='//*[@id="username"]'
-password_xpath='//*[@id="password"]'
-
-credentials_submit_xpath='//*[@id="btn-eventId-proceed"]'
-
-register_icon_xpath='//*[@id="react-portal-root"]/div/div[1]/div[2]/div[2]/div[4]/div/div[2]/div[2]/div[2]'
-class_schedule_xpath='//*[@id="react-portal-root"]/div/div[1]/div[2]/div[2]/div/div/div[1]/div[1]/div/div/div[2]/a'
-fall2022_xpath='/html/body/div[3]/form/table/tbody/tr/td/select/option[2]'
-terms_submit_xpath='/html/body/div[3]/form/input[2]'
